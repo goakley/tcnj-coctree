da4df25c1c223ddab5d4b2484e18a385c5ba9a5f
goakley <goakley123@gmail.com>
2012-11-18 19:57:06 +0000

    Added Doxygen syntax to the octree header

--- octree.h
+++ octree.h
@@ -2,7 +2,9 @@
 #define OCTREE_H
 
 
-/* A generic 3-point space vector */
+/* \brief A 3-point float vector
+ * A generic vector that holds 3 floating-point values (x,y,z).
+ */
 typedef struct Vector3f {
   float x; /* x-coord */
   float y; /* y-coord */
@@ -10,7 +12,11 @@ typedef struct Vector3f {
 } Vector3f;
 
 
-/* A node of an octree, including its root and leaves */
+/* \brief A node of an Octree
+ * A node that can represent any node in an Octree.  A node with no children 
+ * is a leaf, and has a position attribute associated with it.  Nodes with 
+ * children (non-leaves) do not have a position associated with them.
+ */
 typedef struct OctreeNode3f {
   /* Stores pointers to each of the 8 possible children of this octree node */
   struct OctreeNode3f* children[8];
@@ -29,30 +35,56 @@ typedef struct OctreeNode3f {
 } OctreeNode3f;
 
 
-/* Allocates and initializes the memory for a node that is bounded by 
- * the two specified points */
-OctreeNode3f* OctreeNode3f_malloc(Vector3f, Vector3f);
+/* \brief Mallocs an OctreeNode3f
+ * Allocates memory for an OctreeNode3f and initializes its attributes to 
+ * their base value.  The bounds of this node is defined by the two arguments 
+ * to this function, which define the location and size of the node's 
+ * bounding cuboid.
+ * \param point1 A point that defines the bounding cuboid for this node.
+ * \param point2 A point that defines the bounding cuboid for this node.
+ * \return A malloced OctreeNode3f; NULL if there was no room in memory for 
+ *         the node.
+ */
+OctreeNode3f* OctreeNode3f_malloc(Vector3f point1, Vector3f point2);
 
-/* Frees the memory entirely for the specified node */
+/* \brief Frees an OctreeNode3f
+ * Frees the memory for an OctreeNode3f as well as its children (recursively). 
+ * Note that if the node contains a user-defined value, that value is 
+ * NOT FREED.
+ * \param node The node to free from memory.
+ */
 void OctreeNode3f_free(OctreeNode3f *node);
 
-/* Insert a position into the tree with the specified root node
- * RETURNS - 0 on failure
- *           Number of elements in the tree rooted at the specified node
+/* \brief Inserts a position into a node
+ * Inserts a position into the OctreeNode3f.  The user can also specify a 
+ * pointer to some data that will be associated with the position being 
+ * inserted.
+ * \param node The root of the tree in which to insert the poisition.
+ * \param position The position to insert into the tree.
+ * \param usrval The user value to associate with the specified position.  If 
+ *               NULL, no data will be associated.
+ * \return The number of elements inside the tree rooted at the specified node.
  */
-int OctreeNode3f_insert(OctreeNode3f *node, Vector3f, void *);
+int OctreeNode3f_insert(OctreeNode3f *node, Vector3f poisition, void *usrval);
 
-/* Traverses the entire tree rooted at the specified node, calling the 
- * f_node or f_leaf function when appropriate */
+/* \brief Traverses a tree in preorder
+ * Traverses the entire tree rooted at the specified node in preorder.
+ * \param node The root of the tree to traverse
+ * Not sure how to document function pointers...
+ * \return 0 if an error has occured, otherwise non-zero
+ */
 int OctreeNode3f_preorder(OctreeNode3f *node, 
 			  void (*f_node)(OctreeNode3f*), 
 			  void (*f_leaf)(OctreeNode3f*));
 
-/* Traverses the entire tree rooted at the specified node, calling the 
- * f_node or f_leaf function when appropriate */
+/* \brief Traverses a tree in postorder
+ * Traverses the entire tree rooted at the specified node in postorder.
+ * \param node The root of the tree to traverse
+ * Not sure how to document function pointers...
+ * \return 0 if an error has occured, otherwise non-zero
+ */
 int OctreeNode3f_postorder(OctreeNode3f *node, 
 			   void (*f_node)(OctreeNode3f*), 
 			   void (*f_leaf)(OctreeNode3f*));
-
 
 #endif
