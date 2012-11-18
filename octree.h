9fc08a3cd5b2e02112ed33957a9950b01eb05d1e
goakley <goakley123@gmail.com>
2012-11-18 23:41:26 +0000

    Placed the Vector3f definition in its own file

--- octree.h
+++ octree.h
@@ -1,15 +1,7 @@
 #ifndef OCTREE_H
 #define OCTREE_H
 
-
-/* \brief A 3-point float vector
- * A generic vector that holds 3 floating-point values (x,y,z).
- */
-typedef struct Vector3f {
-  float x; /* x-coord */
-  float y; /* y-coord */
-  float z; /* z-coord */
-} Vector3f;
+#include "vectors.h"
 
 
 /* \brief A node of an Octree
---
+++ vectors.h
@@ -0,0 +1,14 @@
+#ifndef VECTORS_H
+#define VECTORS_H
+
+
+/* \brief A 3-point float vector
+ * A generic vector that holds 3 floating-point values (x,y,z).
+ */
+typedef struct Vector3f {
+  float x; /* x-coord */
+  float y; /* y-coord */
+  float z; /* z-coord */
+} Vector3f;
+
+#endif
